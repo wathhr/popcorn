c7b824e0af70b86dae61ea0c711c774af41cad16
wathhr <72303904+wathhr@users.noreply.github.com>
2023-03-20 19:05:08 +0200

    Improve bundle script

--- scripts/bundle.mjs
+++ scripts/bundle.mjs
@@ -4,40 +4,60 @@ import * as esbuild from 'esbuild';
 const options = {
   externalModules: { type: 'boolean' },
   minify: { type: 'boolean' },
-  type: { type: 'string' },
+  types: { type: 'string' },
   watch: { type: 'boolean' },
 };
 const {
-  values: { externalModules = false, minify = false, type, watch = false },
+  values: {
+    externalModules = false,
+    minify = process.env.NODE_ENV === 'production',
+    types = '',
+    watch = false,
+  },
 } = parseArgs({ options });
 
+const actualTypes = [];
+
 const validTypes = ['main', 'preload', 'renderer'];
-if (!validTypes.includes(type)) {
-  console.error(`Invalid type: "${type}"`);
+for (const i in types.split(',')) {
+  const type = types.split(',')[i];
+  if (types === 'all') {
+    actualTypes.push(...validTypes);
+    break;
+  }
+
+  if (!validTypes.includes(type)) console.warn(`Invalid type: ${type}`);
+  else actualTypes.push(type);
+}
+
+if (!actualTypes) {
+  console.warn('No types specified');
   process.exit(1);
 }
 
-const esbuildOptions = {
-  entryPoints: [
-    {
-      in: `./src/${type}/index.ts`,
-      out: type,
-    },
-  ],
-  bundle: true,
-  platform: 'node',
-  format: type === 'renderer' ? 'esm' : 'cjs',
-  minify: minify,
-  write: true,
-  outdir: 'dist',
-  external: ['electron', ...(externalModules ? ['./node_modules/*'] : [])],
-  logLevel: 'info',
-};
+const builds = [];
+for (const type of actualTypes) {
+  const options = {
+    entryPoints: [
+      {
+        in: `./src/${type}/index.ts`,
+        out: type,
+      },
+    ],
+    bundle: true,
+    platform: 'node',
+    format: type === 'renderer' ? 'esm' : 'cjs',
+    minify: minify,
+    write: true,
+    outdir: 'dist',
+    external: ['electron', ...(externalModules ? ['./node_modules/*'] : [])],
+    logLevel: 'info',
+  };
 
-if (watch) {
-  const context = await esbuild.context(esbuildOptions);
-
-  await context.watch();
-} else {
-  await esbuild.build(esbuildOptions);
+  builds.push(options);
 }
+
+builds.map(async (context) => {
+  if (watch) return (await esbuild.context(context)).watch();
+  return esbuild.build(context);
+});
