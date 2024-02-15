7164731c6b10561560e5fd1fc8a2bc983e612955
wathhr <72303904+wathhr@users.noreply.github.com>
2024-02-15 22:56:23 +0200

    chore: remove useless util type

--- types/utils.d.ts
+++ types/utils.d.ts
@@ -2,12 +2,8 @@ type TODO = any;
 
 type ForceArr<T> = T extends any[] ? T : [T];
 
-type CapitalizeLiteral<S extends string> = S extends `${infer U}${infer R}`
-  ? `${U extends U ? Uppercase<U> : U}${R}`
-  : S;
-
 type CamelToScreamingSnakeCase<S extends string> = S extends `${infer T}${infer U}`
-  ? `${T extends CapitalizeLiteral<T> ? '_' : ''}${CapitalizeLiteral<T>}${CamelToScreamingSnakeCase<U>}`
+  ? `${T extends Capitalize<T> ? '_' : ''}${Capitalize<T>}${CamelToScreamingSnakeCase<U>}`
   : S;
 
 type EventName<T> = T extends string
